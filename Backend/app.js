696341230b2ad1eeafe716a5a3854d73913810e2
nathbrl <70270233+nathbrl@users.noreply.github.com>
2022-02-21 16:24:41 +0100

    ajout backend
    création et ajout du backend
    étapes 1, 2 fini étape 3 en cours

---
+++ Backend/app.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require('path');
+
+const saucesRoutes = require('./routes/sauces')
+
+const userRoutes = require('./routes/user')
+
+mongoose.connect("mongodb+srv://nbrl:lolilol@cluster0.4xyys.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    { useNewUrlParser: true, useUnifiedTopology: true })
+    .then( () => console.log("Connexion à MongoDB réussie !"))
+    .catch( () => console.log("Connexion à MongoDB échouée !"));
+
+const app = express();
+
+app.use((req, res, next) => {
+    //permet d'accéder à l'API depuis n'importe quelle origine
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    //permet d'ajouter les headers mentionnés aux requêtes envoyées vers notre API
+    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization");
+    //permet d'envoyer des requêtes avec les méthodes mentionnées ( GET, POST, etc...)
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
+    next();
+});
+
+//Donne accès au corps de la requête et rend ses données exploitables
+//anciennement avec body-parser
+app.use(express.json());
+
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+app.use('/api/sauces', saucesRoutes);
+app.use('/api/auth', userRoutes);
+
+module.exports = app;
---
+++ Backend/routes/sauces.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+
+const saucesCtrl = require('../controllers/sauces');
+const auth = require('../middleware/auth');
+const multer = require('../middleware/multer-config')
+
+router.post("/", auth, multer, saucesCtrl.createSauce);
+router.put('/:id', auth, saucesCtrl.modifySauce);
+router.delete('/:id', auth, saucesCtrl.deleteSauce);
+router.get('/:id', auth, saucesCtrl.getOneSauce);
+router.get('/', auth, saucesCtrl.getAllSauces);
+
+module.exports = router;
