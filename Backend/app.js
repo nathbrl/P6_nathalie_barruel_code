b4b5c205861b171393cf7fbe4068f9a1c3eb00b2
nathbrl <70270233+nathbrl@users.noreply.github.com>
2022-03-14 14:11:12 +0100

    ajout helmet
    ajout d'helmet

--- Backend/app.js
+++ Backend/app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const mongoose = require("mongoose");
 const path = require('path');
 
+const helmet = require('helmet');
+
 const sauceRoutes = require('./routes/sauces');
 
 const userRoutes = require('./routes/user')
@@ -14,18 +16,16 @@ mongoose.connect("mongodb+srv://nbrl:lolilol@cluster0.4xyys.mongodb.net/myFirstD
 const app = express();
 
 app.use((req, res, next) => {
-    //permet d'accéder à l'API depuis n'importe quelle origine
     res.setHeader("Access-Control-Allow-Origin", "*");
-    //permet d'ajouter les headers mentionnés aux requêtes envoyées vers notre API
     res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization");
-    //permet d'envoyer des requêtes avec les méthodes mentionnées ( GET, POST, etc...)
     res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
+    res.setHeader('Cross-Origin-Resource-Policy', 'same-site');
     next();
 });
 
-//Donne accès au corps de la requête et rend ses données exploitables
-//anciennement avec body-parser
 app.use(express.json());
+
+app.use(helmet());
 
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
