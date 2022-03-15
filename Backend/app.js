0cf3f2cfa8c97c87a16b0debcbe0654a76ebdfd0
nathbrl <70270233+nathbrl@users.noreply.github.com>
2022-03-15 12:00:35 +0100

    Réglage soucis helmet
    Re utilisation d'helmet après suppression de la BDD puis création d'une BDD neuve

--- Backend/app.js
+++ Backend/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const path = require('path');
+const helmet = require('helmet');
 require("dotenv").config();
 
 const sauceRoutes = require('./routes/sauces');
@@ -8,17 +9,22 @@ const userRoutes = require('./routes/user')
 
 const app = express();
 
+//protection des headers
+app.use(helmet());
+
 //connexion BDD
 mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${process.env.DB_NAME}?retryWrites=true&w=majority`,
     { useNewUrlParser: true, useUnifiedTopology: true })
     .then( () => console.log("Connexion à MongoDB réussie !"))
     .catch( () => console.log("Connexion à MongoDB échouée !"));
 
+
 //Paramétrage des headers
 app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization");
     res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
+    res.setHeader('Cross-Origin-Resource-Policy', 'cross-origin');
     next();
 });
 
